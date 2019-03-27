78ad823698890761c84de54d8cafd0f3dbf8fc27
Roman Sakh <gribochechek@gmail.com>
2019-03-27 13:02:43 +0200

    minor fixes

--- src/main/java/controller/crypter/SecurityEntity.java
+++ src/main/java/controller/crypter/SecurityEntity.java
@@ -5,11 +5,24 @@ import model.entity.Entity;
 public class SecurityEntity<T extends Entity> {
     private int fakeId;
 
-    private T entity;
+    T entity;
 
     public SecurityEntity(int fakeId, T entity) {
         this.fakeId = fakeId;
         this.entity = entity;
+    }
+
+    public SecurityEntity(T entity) {
+        this.fakeId = entity.hashCode();
+        this.entity = entity;
+    }
+
+    public int getFakeId() {
+        return fakeId;
+    }
+
+    public void setFakeId(int fakeId) {
+        this.fakeId = fakeId;
     }
 
     public T getEntity() {
