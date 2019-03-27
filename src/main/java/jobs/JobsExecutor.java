6f58fd8f3dba58ef53e734911de26ad99fbf2259
Roman Sakh <gribochechek@gmail.com>
2019-03-27 14:56:00 +0200

    Last Stable version

--- src/main/java/jobs/JobsExecutor.java
+++ src/main/java/jobs/JobsExecutor.java
@@ -5,13 +5,25 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class JobsExecutor {
-
+    /**
+     * Adjusts the time period in hours for counting income
+     */
     private static final int HOURS = 0;
+    /**
+     * Adjusts the time period in minutes for counting income
+     */
     private static final int MINUTES = 1;
+    /**
+     * Adjusts the time period in seconds for counting income
+     */
     private static final int SECONDS = 0;
     private static long summarySeconds;
     private static ScheduledExecutorService executorService;
 
+    /**
+     * This method starts the job which counts deposit income and credit indebtedness. Jobs works in background in
+     * separate thread. Frequency of counting adjusted in class constants
+     */
 
     public static void startJobs() {
         long hoursToSeconds = TimeUnit.HOURS.toSeconds(HOURS);
